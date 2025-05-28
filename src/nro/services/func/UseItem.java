e3c3bd92c3ee758c15acfbbae9d5bbb76016e669
LAPTOP-LJC6KU1H\khail <khaile.huynhduy@gmail.com>
2025-05-28 22:29:44 +0700

    backupdb

--- src/nro/services/func/UseItem.java
+++ src/nro/services/func/UseItem.java
@@ -1538,7 +1538,7 @@ public class UseItem {
 
                         //khaile add
                         case 1698: { // linh tuu
-                            long amount = 500_000_000_000L;
+                            long amount = 5_000_000_000L;
                             pl.nPoint.powerUp(amount);
                             pl.nPoint.tiemNangUp(amount);
                             PlayerService.gI().sendTNSM(pl, (byte) 2, amount);
